665e15a78673af898474818b4810111fd9a16dd2
Tcphuoc <tcphuoc0511@gmail.com>
2023-10-25 17:20:03 +0700

    Create login page - Remove devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: { sessions: 'users/sessions', registrations: 'users/registrations',
                             passwords: 'users/passwords' }
-  devise_scope :user do
-    get 'sign_in', to: 'users/sessions#new'
-    get 'sign_up', to: 'users/registrations#new'
-    get 'forget_password', to: 'users/passwords#new'
-  end
-
   get '/home', to: 'pages#home'
   get 'pages/about'
   root 'pages#home'
