2b39c660a8ad5b0d672dccac25472a04550e8781
Tcphuoc <tcphuoc0511@gmail.com>
2023-10-25 17:03:58 +0700

    Create signup page - Add constant file Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
+  end
   get '/home', to: 'pages#home'
   get 'pages/about'
   root 'pages#home'
