8fa041db86d436ffe194123ed2f7e94c07343d7c
Tcphuoc <tcphuoc0511@gmail.com>
2023-10-31 09:01:33 +0700

    CRUD Category, Format Product

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,55 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    category = Category.new(category_params)
+    if category.save
+      flash[:notice] = 'Create category success'
+      redirect_to categories_url
+    else
+      flash.now[:alert] = 'Create category fail. Please try again'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   def show
     @category = Category.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = 'Update category success'
+      redirect_to categories_url
+    else
+      flash.now[:alert] = 'Update category fail. Please try again'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:notice] = 'Delete category success'
+    redirect_to categories_url
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :shop_id)
   end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,8 +2,4 @@ module ProductsHelper
   def display_price(price)
     "#{number_with_delimiter(price, delimeter: ',')} vnđ"
   end
-
-  def all_products
-    Product.all
-  end
 end
