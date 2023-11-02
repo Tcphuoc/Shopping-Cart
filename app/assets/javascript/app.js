bfe65f0423d5928cfda2ddefa31f4895bb75c4a1
Tcphuoc <tcphuoc0511@gmail.com>
2023-11-02 20:19:07 +0700

    Config Jquery

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,4 +1,4 @@
-//= require libs/bootstrap.bundle.min.js
-//= require libs/jquery-3.7.1.slim.min.js
+//= require libs/jquery-3.7.0.min.js
+//= require libs/jquery-3.x-git.min.js
 //= require libs/sweetalert2@11.js
 //= require jquery-ui
